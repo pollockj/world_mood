f624883fe19dfb0139c9c2a2e7c1ff24c347d4e5
Wesley Pollock <wesleypollock@Wesleys-MacBook-Pro.local>
2015-12-31 14:06:00 -0700

    Add correctly get retweet text

--- tweet_to_csv.py
+++ tweet_to_csv.py
@@ -3,10 +3,6 @@ from nltk.tokenize import TweetTokenizer
 import csv
 import sys
 import argparse
-import getopt
-
-def usage():
-	print 'tweet_to_csv.py [OPTIONS] -i <input_file> -o <output_file>'
 
 #Given a tweet, get_hashtags will return a list of the hashtags (text).
 #If there are no hashtags, it will return False
@@ -20,7 +16,6 @@ def get_hashtags(tweet):
 	return tmp_list
 
 def get_parser():
-
 	parser = argparse.ArgumentParser(description="Storge data from JSON Tweet to .csv")
 	parser.add_argument('-i',
 						'--input',
@@ -53,20 +48,27 @@ def main(args):
 	f = csv.writer(out_file)
 
 	#Write headers for .csv
-	fields = ['Text', 'hashtags']
+	fields = ['Text', 'hashtags', 'retweet?']
 	f.writerow(fields)
 
 	#loop for lines in .json (assuming each line is a tweet)
 	for line in in_file:
+		retweet = False
 		tweet = json.loads(line)
 
-		#Get tweet text, and tokenize
-		if tweet.get('text'):
+		#Check to see if tweet is a retweet 
+		if tweet.get("retweeted_status"):
+			text = tweet.get("retweeted_status").get("text").encode("unicode_escape") #store retweet text
+			retweet = True
+		#Else get tweet text
+		elif tweet.get('text'):
 			text = tweet.get('text').encode('unicode_escape')
-			if args.token:
-				processed_text = TweetTokenizer().tokenize(text)
-			else:
-				processed_text = text
+			retweet = False
+		#Tokenize if necessary
+		if args.token:
+			processed_text = TweetTokenizer().tokenize(text)
+		else:
+			processed_text = text
 		#print(processed_tweet)
 
 		#Get favorite and retweet count
@@ -76,15 +78,15 @@ def main(args):
 		#Write data to csv
 		entities = tweet.get("entities")
 		if entities: #if entities != None
-
 			#HASHTAGS
 			hashtag_list = get_hashtags(tweet)
 
 			if not args.links:
 				if entities.get("urls") == [] or not entities.get("media"): #if no urls or media
-					f.writerow([processed_text, hashtag_list])
+					f.writerow([processed_text, hashtag_list, retweet])
 			else:
-				f.writerow([processed_text, hashtag_list])
+				f.writerow([processed_text, hashtag_list, retweet])
+
 
 	#Close files when done
 	out_file.close()
